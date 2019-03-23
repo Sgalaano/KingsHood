e0d6cc21f15e8e09b1bafbfbaaed351461c30321
nignanthomas <nignanthomas@gmail.com>
2019-03-23 05:22:38 +0300

    create index view function

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+def index(request):
+    current_user = request.user
+    profile = Profile.objects.get(user = current_user)
+
+    posts = Post.objects.filter(neighborhood = profile.neighborhood)
+    businesses = Business.objects.filter(neighborhood = profile.neighborhood)
+    hood = profile.neighborhood
+
+    return render(request,'index.html', locals())
